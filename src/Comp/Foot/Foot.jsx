dc66a64f8a5ddf4ee68307c4df7b61f01eb7fdc6
Kraftifyweb <kraftifyweb@gmail.com>
2026-02-13 07:47:14 +0530

    added jsx

---
+++ src/Comp/Foot/Foot.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Foot() {
+  return (
+    <div className='p'><h3>Copyright @ Maan Bahadur Bohara</h3></div>
+  )
+}
+
+export default Foot
---
+++ src/Comp/Head/Head.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './head.css'
+
+function Head() {
+  return (
+    <div className='p'><h1>Maan Bahadur Bohara</h1></div>
+  )
+}
+
+export default Head
