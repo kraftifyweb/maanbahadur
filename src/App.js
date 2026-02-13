9ac47cd8a76fe16d25ee9de4ed186eda9d14bf89
Kraftifyweb <kraftifyweb@gmail.com>
2026-02-13 07:43:33 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Foot from './Comp/Foot/Foot';
+import Head from './Comp/Head/Head';
 
 function App() {
   return (
     <div className="App">
+      <Head />
       <header className="App-header">
         <h1>Maan Bahadur Bohara</h1>
         <h2>i am from nepal</h2>
       </header>
+      <Foot />
     </div>
   );
 }
