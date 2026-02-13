acc27662ceed451ad635856981a5439c5fdd1451
Kraftifyweb <kraftifyweb@gmail.com>
2026-02-13 07:30:55 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Maan Bahadur Bohara</h1>
+        <h2>i am from nepal</h2>
       </header>
     </div>
   );
