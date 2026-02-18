258ce10b0fa09a89fb95b684597005cab6a5732e
Kraftifyweb <kraftifyweb@gmail.com>
2026-02-18 08:34:07 +0530

    added text

--- src/Comp/Contact/Contact.jsx
+++ src/Comp/Contact/Contact.jsx
@@ -33,8 +33,8 @@ function Contact() {
       </Row>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Label>Enter Your Message</Form.Label>
+        <Form.Control as="textarea" rows={3} placeholder="Enter Your Message"/>
       </Form.Group>
 
       <Form.Group className="mb-3" id="formGridCheckbox">
